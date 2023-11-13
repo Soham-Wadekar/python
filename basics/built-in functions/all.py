1d085f0f44cd513e7ecdce463dd8a2f92899b090
sohamw03@gmail.com <donbhai@yahoo.com>
2023-11-13 22:50:40 +0530

    Built-in functions (A) added

---
+++ basics/built-in functions/all.py
@@ -0,0 +1,16 @@
+# all() - Returns True if all elements of the iterable are true (of if the iterable is empty)
+
+a = [1,0,1,2,5]
+print(all(a))
+
+b = [True,True,1,2,""]
+print(all(b))
+
+c = [[1,2],[False]]
+print(all(c))
+
+d = [[0,0],[0,0],[0,0]]
+print(all(d))
+
+e = [[],True,[1]]
+print(all(e))
---
+++ basics/built-in functions/any.py
@@ -0,0 +1,16 @@
+# any() - Returns True if any element of the iterable are true (of if the iterable is empty)
+
+a = [1,0,1,2,5]
+print(any(a))
+
+b = [True,True,1,2,""]
+print(any(b))
+
+c = [[1,2],[False]]
+print(any(c))
+
+d = [[0,0],[0,0],[0,0]]
+print(any(d))
+
+e = [[],False,0]
+print(any(e))
