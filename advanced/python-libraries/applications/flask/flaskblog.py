66e9abd218dea652d6921e0d38e3fd13cd49336b
sohamw03@gmail.com <donbhai@yahoo.com>
2024-10-23 15:59:54 +0530

    forms in flask

--- advanced/python-libraries/applications/flask/flaskblog.py
+++ advanced/python-libraries/applications/flask/flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -21,7 +21,7 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
     return render_template('home.html', posts=posts)
 
 
@@ -29,14 +29,23 @@ def hello():
 def about():
     return render_template('about.html', title="About")
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title="Register", form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@gmail.com" and form.password.data == "12345678":
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid email address or password', 'danger')
     return render_template('login.html', title="Login", form=form)
 
 
