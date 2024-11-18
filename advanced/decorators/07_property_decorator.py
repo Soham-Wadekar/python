7c878affe764934b107c6f29316fd97aa9045eee
sohamw03@gmail.com <donbhai@yahoo.com>
2024-11-18 10:59:52 +0530

    additional decorator functionality added

---
+++ advanced/decorators/07_property_decorator.py
@@ -0,0 +1,54 @@
+'''
+
+'''
+
+class Employee:
+
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    @property
+    def email(self):
+        return f"{self.first.lower()}.{self.last.lower()}@email.com"
+
+    @property
+    def fullname(self):
+        return f"{self.first} {self.last}"
+    
+    @fullname.setter
+    def fullname(self, fullname):
+        first, last = fullname.split(' ')
+        self.first = first
+        self.last = last
+
+    @fullname.deleter
+    def fullname(self):
+        print(f"Deleting {self.fullname}")
+        self.first, self.last = None, None
+    
+emp_1 = Employee("Dwight", "Schrute")
+
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
+print()
+
+emp_1.first = "Dwayne"
+
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
+print()
+
+emp_1.fullname = "Jim Halpert"
+
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
+print()
+
+del emp_1.fullname
+
+print(emp_1.first)
+print(emp_1.last)
