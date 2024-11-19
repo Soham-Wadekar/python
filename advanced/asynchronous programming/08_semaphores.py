fece5b21b2e85ddacb673f7a95e9fb891a4052b6
sohamw03@gmail.com <donbhai@yahoo.com>
2024-11-19 23:17:32 +0530

    asynchronous programming added

---
+++ advanced/asynchronous programming/08_semaphores.py
@@ -0,0 +1,19 @@
+'''
+Semaphores are similar to locks but while locks allow only 1 task to access the resource,
+semaphores allow a specified number of tasks to access the shared resource concurrently
+'''
+
+import asyncio
+
+async def access_shared_resource(semaphore, resource_id):
+    async with semaphore:
+        # Simulate accessing a limited resource
+        print(f"Accessing resource: {resource_id}")
+        await asyncio.sleep(2)
+        print(f"Releasing resource: {resource_id}")
+
+async def main():
+    semaphore = asyncio.Semaphore(2)            # Allow 2 concurrent access
+    await asyncio.gather(*(access_shared_resource(semaphore, i) for i in range(5)))
+
+asyncio.run(main())
