e1f0c0886d2a0818acd8e164558315c515941d68
sohamw03@gmail.com <donbhai@yahoo.com>
2025-07-21 15:03:17 +0530

    added templates and databases

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        "author": "John Doe",
+        "title": "Blog Post 1",
+        "content": "This is the first blog post",
+        "date_posted": "July 17, 2025",
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog Post 2",
+        "content": "This is the second blog post",
+        "date_posted": "July 2, 2025",
+    },
+    {
+        "author": "John Doe",
+        "title": "Blog Post 3",
+        "content": "This is the third blog post",
+        "date_posted": "July 19, 2024",
+    },
+]
 
 
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
+
 
 def about(request):
-    return HttpResponse("<h1>About Page</h1>")
+    return render(request, "blog/about.html", {"title": "About"})
