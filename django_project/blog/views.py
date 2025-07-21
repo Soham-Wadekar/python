e2b4c27be4fac030b8c37266fb58f16b1ec89be8
sohamw03@gmail.com <donbhai@yahoo.com>
2025-07-21 20:28:29 +0530

    added databases

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,29 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        "author": "John Doe",
-        "title": "Blog Post 1",
-        "content": "This is the first blog post",
-        "date_posted": "July 17, 2025",
-    },
-    {
-        "author": "Jane Doe",
-        "title": "Blog Post 2",
-        "content": "This is the second blog post",
-        "date_posted": "July 2, 2025",
-    },
-    {
-        "author": "John Doe",
-        "title": "Blog Post 3",
-        "content": "This is the third blog post",
-        "date_posted": "July 19, 2024",
-    },
-]
+from .models import Post
 
 
 def home(request):
-    context = {"posts": posts}
+    context = {"posts": Post.objects.all()}
     return render(request, "blog/home.html", context)
 
 
